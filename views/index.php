5f9556ac10c9f6fb0f6cc98ebc8f7f82dca7137d
Paola Ugalde <146759096+AnyaFigue@users.noreply.github.com>
2024-11-04 22:38:40 -0600

    Main
    Se puso el main y se le dio estilo

--- views/index.php
+++ views/index.php
@@ -4,6 +4,8 @@
 <body>
     <?php include ("./assets/fragmentos/header.php"); ?>
 
+    <?php include ("./assets/fragmentos/main.php"); ?>
+
     <?php include ("./assets/fragmentos/footer.php"); ?>
 </body>
     <?php include ("./assets/fragmentos/scripts.php"); ?>
