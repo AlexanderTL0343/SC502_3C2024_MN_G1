e362acb5dac11eed347b0bee2b29dfa1b5052be4
NathanCTR <157663583+NathanCTR@users.noreply.github.com>
2024-11-05 01:43:26 -0600

    About Us
    Página de acerca de nosotros

---
+++ SC502_3C2024_MN_G1/views/about.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include ("./assets/fragmentos/head.php"); ?>//Funcionan como fragmento
+<body>
+    <?php include ("./assets/fragmentos/header.php"); ?>
+
+    <?php include ("./assets/fragmentos/aboutus.php"); ?>
+
+    <?php include ("./assets/fragmentos/footer.php"); ?>
+</body>
+    <?php include ("./assets/fragmentos/scripts.php"); ?>
+</html>
