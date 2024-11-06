3fb1ab94834ed83bb19a4f039d4b1f2fb6877c9c
Crowned08 <87036156+Crowned08@users.noreply.github.com>
2024-11-05 18:28:40 -0600

    Creacion login

---
+++ SC502_3C2024_MN_G1/SC502_3C2024_MN_G1/views/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include ("./assets/fragmentos/head.php"); ?>//Funcionan como fragmento
+<body>
+    <?php include ("./assets/fragmentos/header.php"); ?>
+
+    <?php include ("./assets/fragmentos/plogin.php"); ?>
+
+    <?php include ("./assets/fragmentos/footer.php"); ?>
+</body>
+    <?php include ("./assets/fragmentos/scripts.php"); ?>
+</html>
