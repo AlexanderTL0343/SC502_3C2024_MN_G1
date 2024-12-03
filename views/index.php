ba777ef915d3e5d0b9887d5a692f07fcd5ec1ef6
Brandon-0717 <brandon0717.cr@gmail.com>
2024-12-03 01:33:06 -0600

    Se mueve la validacion del header por rol a un archivo en config

--- views/index.php
+++ views/index.php
@@ -3,21 +3,8 @@
 <html lang="es">
 <?php include ("./assets/fragmentos/head.php"); ?>
 <body>
-<?php
-    if (!isset($_SESSION['usuario'])) {
-        // Redirige al login si no hay sesión activa
-        include("./assets/fragmentos/header.php");
 
-    } elseif ($_SESSION['usuario']['nombreRol'] === 'ADMIN') {//donde dice [usuario] es la variable de sesion, esta variable almacena un arreglo asociativo
-        include("./assets/fragmentos/header_admin.php");      //[nombreRol] es la llave en el arreglo, es deci (llave=>valor), entonces buscamos el valor de nombreRol
-    } elseif ($_SESSION['usuario']['nombreRol'] === 'RECLUTADOR') {
-        include("./assets/fragmentos/header_reclutador.php");
-    } elseif ($_SESSION['usuario']['nombreRol'] === 'POSTULANTE') {
-        include("./assets/fragmentos/header_postulante.php");
-    } elseif ($_SESSION['usuario']['nombreRol'] === '') {
-        include("./assets/fragmentos/header.php");
-    }
-?>
+<?php include("../config/session.php");?> <!--PARA COLOCAR EL HEADER DEPENDIENDO DEL ROL-->
 
     <?php include ("./assets/fragmentos/hero1.php"); ?>
 
