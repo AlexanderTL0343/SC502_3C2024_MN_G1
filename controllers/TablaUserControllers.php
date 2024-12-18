12641203bcc82b75f19da148da3890176f15117b
NathanCTR <157663583+NathanCTR@users.noreply.github.com>
2024-12-18 02:47:54 -0600

    Eliminar
    Opciones de eliminar

---
+++ controllers/TablaUserControllers.php
@@ -0,0 +1,90 @@
+<?php
+include '../models/TablaUser.php';
+
+switch ($_GET['op']) {
+    case 'LlenarTablaUser':
+        $tabla = new TablaUser();
+        $clientes = $tabla->listarTablaUser();
+        $data = array();
+        foreach ($clientes as $reg) {
+            $data[] = array(
+                "0" => $reg->getId(),
+                "1" => $reg->getNombre(),
+                "2" => $reg->getEdad(),
+                "3" => $reg->getEmail(),
+                "4" => $reg->getProfesion(),
+                "5" => $reg->getFechaRegistro(),
+                "6" => $reg->getIdRol(),
+                "7" => $reg->getEstado(),
+                "8" => '<button class="btn btn-warning" id="modificarUsuario">Modificar</button>  '.
+                '<button class="btn btn-danger" id="eliminarUsuario">Eliminar</button> '
+                        
+            );
+        }
+        $resultados = array(
+            "sEcho" => 1,
+            "iTotalRecords" => count($data),
+            "iTotalDisplayRecords" => count($data),
+            "aaData" => $data
+        );
+        echo json_encode($resultados);
+        break;
+        case 'editar':
+            $id = isset($_POST["Eid"]) ? trim($_POST["Eid"]) : "";
+
+            $email = isset($_POST["Eemail"]) ? trim($_POST["Eemail"]) : "";
+            $nombre = isset($_POST["Enombre"]) ? trim($_POST["Enombre"]) : "";
+            $edad = isset($_POST["Eedad"]) ? trim($_POST["Eedad"]) : "";
+            $profesion = isset($_POST["Eprofesion"]) ? trim($_POST["Eprofesion"]) : "";
+            $rol = isset($_POST["Erol"]) ? trim($_POST["Erol"]) : "";
+        
+            $usuario = new TablaUser();
+
+            $encontrado = $usuario->verificarExistenciaDb($id);
+
+            if ($encontrado == 1) {
+                $usuario->llenarCampos($id);
+                $usuario->setNombre($nombre);
+                $usuario->setEdad($edad);
+                $usuario->setProfesion($profesion);
+                $usuario->setIdRol($rol);
+                $usuario->setEmail($email);
+        
+                $modificados = $usuario->actualizarUsuario();
+                if ($modificados > 0) {
+                    echo 1;
+                } else {
+                    echo 0;
+                }
+            } else {
+                //print_r("ERROR DE CONTROLADOR");
+
+                echo 2; 
+            }
+        break;
+        case 'eliminar':
+            $id = isset($_POST['id']) ? trim($_POST['id']) : '';
+            error_log('ID recibido en eliminar: ' . $id);
+            if ($id === '') {
+                echo 2; 
+                break;
+            }
+        
+            $usuario = new TablaUser();
+            $encontrado = $usuario->verificarExistenciaDb($id);
+        
+            if ($encontrado == 1) {
+                $eliminado = $usuario->eliminarUsuario($id);
+        
+                if ($eliminado > 0) {
+                    echo 1; 
+                } else {
+                    echo 0; 
+                }
+            } else {
+                echo 2; 
+            }
+            break;
+        
+}
+?>
---
+++ models/TablaPublicaciones.php
@@ -0,0 +1,232 @@
+<?php
+session_start();
+require_once '../config/Conexion.php';
+
+class TablaPubli extends Conexion
+{
+
+    protected static $cnx;
+    private $idPublicacionPk;
+    private $idUsuarioFk;
+    private $estado;
+    private $tituloPublicacion;
+    private $descripcion;
+    private $fechaPublicacion;
+    private $ubicacion;
+    private $precioAprox;
+
+    public function __construct() {}
+
+    //Metodos de conexion y desconexion 
+
+    public static function getConexion()
+    {
+        self::$cnx = Conexion::conectar();
+    }
+
+    public static function desconectar()
+    {
+        self::$cnx = null;
+    }
+
+    // metodos set y get 
+
+    public  function  getIdPublicacionesPk()
+    {
+        return $this->idPublicacionPk;
+    }
+
+    public  function  getIdUsuarioFk()
+    {
+        return $this->idUsuarioFk;
+    }
+    public  function  getEstado()
+    {
+        return $this->estado;
+    }
+    public  function  getTituloPublicacion()
+    {
+        return $this->tituloPublicacion;
+    }
+    public  function  getDescripcion()
+    {
+        return $this->descripcion;
+    }
+    public  function  getFechaPublicacion()
+    {
+        return $this->fechaPublicacion;
+    }
+    public  function  getUbicacion()
+    {
+        return $this->ubicacion;
+    }
+    public  function  getPrecioAprox()
+    {
+        return $this->precioAprox;
+    }
+
+    public function setIdPublicacionesPk($idPublicacionPk){
+        $this->idPublicacionPk = $idPublicacionPk;
+    }
+    public function setIdUsuarioFk($idUsuarioFk){
+        $this->idUsuarioFk = $idUsuarioFk;
+    }
+    public function setEstado($estado){
+        $this->estado = $estado;
+    }
+    public function setTituloPublicacion($tituloPublicacion){
+        $this->tituloPublicacion = $tituloPublicacion;
+    }
+    public function setDescripcion($descripcion){
+        $this->descripcion = $descripcion;
+    }
+    public function setFechaPublicacion($fechaPublicacion){
+        $this->fechaPublicacion = $fechaPublicacion;
+    }
+    public function setUbicacion($ubicacion){
+        $this->ubicacion = $ubicacion;
+    }
+    public function setPrecioAprox($precioAprox){
+        $this->precioAprox = $precioAprox;
+    }
+
+    //funcion para listar la tabla de los usuarios 
+
+    public function listarTablaPubli()
+    {
+        $query = "SELECT p.ID_PUBLICACION_PK, p.ID_USUARIO_FK, p.TITULO_PUBLICACION, p.DESCRIPCION,
+        p.FECHA_PUBLICACION, p.UBICACION, p.PRECIO_APROX, e.NOMBRE_ESTADO
+          FROM publicaciones p
+          JOIN estados e ON p.ID_ESTADO_FK = e.ID_ESTADO_PK";
+        $arr = array();
+        try {
+            self::getConexion();
+            
+            $resultado = self::$cnx->prepare($query);
+            $resultado->execute();
+            self::desconectar();
+            foreach ($resultado->fetchAll() as $encontrado) {
+                $client = new TablaPubli();
+                $client->setIdPublicacionesPk($encontrado['ID_PUBLICACION_PK']);
+                $client->setIdUsuarioFk($encontrado['ID_USUARIO_FK']);
+                $client->setTituloPublicacion($encontrado['TITULO_PUBLICACION']);
+                $client->setDescripcion($encontrado['DESCRIPCION']);
+                $client->setFechaPublicacion($encontrado['FECHA_PUBLICACION']);
+                $client->setUbicacion($encontrado['UBICACION']);
+                $client->setPrecioAprox($encontrado['PRECIO_APROX']);
+                $client->setEstado($encontrado['NOMBRE_ESTADO']);
+                $arr[] = $client;
+            }
+            return $arr;
+        } catch (PDOException $Exception) {
+            self::desconectar();
+            $error = "Error " . $Exception->getCode() . ": " . $Exception->getMessage();
+            return json_encode($error);
+        }
+    }
+
+    public function verificarExistenciaDb($id){
+        $query = "SELECT * FROM publicaciones where ID_PUBLICACION_PK=?";
+     try {
+         self::getConexion();
+            $resultado = self::$cnx->prepare($query);		
+            $resultado->bindParam(1,$id);
+            $resultado->execute();
+            self::desconectar();
+            $encontrado = false;
+
+
+            $nombre=$resultado->fetch();
+            if ($nombre!=null)
+            {
+                $encontrado = true;
+            }
+            return $encontrado;
+           } catch (PDOException $Exception) {
+               self::desconectar();
+               $error = "Error ".$Exception->getCode().": ".$Exception->getMessage();
+             return $error;
+           }
+    }
+
+    public function llenarCampos($id){
+        $query = "SELECT * FROM publicaciones where ID_PUBLICACION_PK=:ID_PUBLICACION_PK";
+        try {
+        self::getConexion();
+        $resultado = self::$cnx->prepare($query);		 	
+        $resultado->bindParam(":ID_PUBLICACION_PK",$id,PDO::PARAM_INT);
+        $resultado->execute();
+        self::desconectar();
+        foreach ($resultado->fetchAll() as $encontrado) {
+            $this->setIdPublicacionesPk($encontrado['ID_PUBLICACION_PK']);
+            $this->setTituloPublicacion($encontrado['TITULO_PUBLICACION']);
+        }
+        } catch (PDOException $Exception) {
+        self::desconectar();
+        $error = "Error ".$Exception->getCode().": ".$Exception->getMessage();;
+        return json_encode($error);
+        }
+    }
+
+    public function actualizarPublicaciones()
+    {
+        $query = "UPDATE publicaciones 
+        SET ID_USUARIO_FK = :ID_USUARIO_FK, 
+            TITULO_PUBLICACION = :TITULO_PUBLICACION, 
+            DESCRIPCION = :DESCRIPCION, 
+            UBICACION = :UBICACION, 
+            PRECIO_APROX = :PRECIO_APROX
+            
+        WHERE ID_PUBLICACION_PK = :ID_PUBLICACION_PK";
+        try {
+            self::getConexion();
+            $id = $this->getIdPublicacionesPk();
+            $usuario = $this->getIdUsuarioFk();
+            $titulo = $this->getTituloPublicacion();
+            $descripcion = $this->getDescripcion();
+            $ubicacion = $this->getUbicacion();
+            $precio = $this->getPrecioAprox();
+        
+            $resultado = self::$cnx->prepare($query);
+            $resultado->bindParam(":ID_PUBLICACION_PK", $id, PDO::PARAM_INT);
+            $resultado->bindParam(":ID_USUARIO_FK", $usuario, PDO::PARAM_INT);
+            $resultado->bindParam(":TITULO_PUBLICACION", $titulo, PDO::PARAM_STR);
+            $resultado->bindParam(":DESCRIPCION", $descripcion, PDO::PARAM_STR);
+            $resultado->bindParam(":UBICACION", $ubicacion, PDO::PARAM_STR);
+            $resultado->bindParam(":PRECIO_APROX", $precio, PDO::PARAM_INT);
+
+            self::$cnx->beginTransaction(); // desactiva el autocommit
+            $resultado->execute();
+            self::$cnx->commit(); // realiza el commit y vuelve al modo autocommit
+            self::desconectar();
+
+            return $resultado->rowCount();
+        } catch (PDOException $Exception) {
+            self::$cnx->rollBack();
+            self::desconectar();
+            $error = "Error " . $Exception->getCode() . ": " . $Exception->getMessage();
+            return $error;
+        }
+    }
+
+
+    public function eliminarPublicacion($id) {
+        try {
+            $sql = "DELETE FROM publicaciones WHERE ID_PUBLICACION_PK = ?";
+            self::getConexion();
+            $stmt = self::$cnx->prepare($sql); 
+            $stmt->bindParam(1, $id, PDO::PARAM_INT);
+            $stmt->execute();
+            $rowCount = $stmt->rowCount(); 
+            self::desconectar();
+            return $rowCount; 
+        } catch (PDOException $e) {
+            self::desconectar();
+            $error = "Error " . $e->getCode() . ": " . $e->getMessage(); 
+            return 0; 
+        }
+    }
+    
+}
+
+?>
